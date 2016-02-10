57cae195c1539420950c477a32101c2227c955d5
Jonathan Abourbih <jabourbi@thoughtworks.com>
2016-02-10 13:30:52 +0000

    test

--- jobs/exampleJobs.groovy
+++ jobs/exampleJobs.groovy
@@ -16,98 +16,98 @@ mavenJob ('test-job') {
 
 
 
-
-
-
-
-
-
-// Multiple build jobs with parameters
-['some-repo', 'another-repo', 'yet-another-repo'].each {
-    job("${it}-build") {
-        wrappers {
-            environmentVariables(
-                    SOME_VARIABLE: 'someValue',
-                    ANOTHER_VARIABLE: 'another-value'
-            )
-        }
-
-        scm {
-            git "https://foo.com/some-group/${it}.git"
-        }
-
-        steps {
-            gradle('clean build')
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Factor out URL generation
-Closure myGit(projectName) {
-    return {
-        git {
-            remote {
-                url "https://foo.com/some-group/${projectName}"
-                credentials '30541887-abf1-4788-b071-d7c801f2beb5'
-            }
-        }
-    }
-}
-
-['some-repo', 'another-repo', 'yet-another-repo'].each {
-    job("${it}-build-with-git-function") {
-        wrappers {
-            environmentVariables(
-                    SOME_VARIABLE: 'someValue',
-                    ANOTHER_VARIABLE: 'another-value'
-            )
-        }
-
-        scm myGit(it)
-
-        steps {
-            gradle('clean build')
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Add stuff to a job
-def myJob = job('my-job-integration')
-SomeUtilities.integrationJob(myJob, '')
-
-
-// Create a view
-listView('integration-jobs') {
-    description('All integration jobs')
-
-    jobs {
-        regex(/^.*-integration$/)
-    }
-}
+//
+//
+//
+//
+//
+//
+//// Multiple build jobs with parameters
+//['some-repo-m', 'another-repo-m', 'yet-another-repo-m'].each {
+//    job("${it}-build") {
+//        wrappers {
+//            environmentVariables(
+//                    SOME_VARIABLE: 'someValue',
+//                    ANOTHER_VARIABLE: 'another-value'
+//            )
+//        }
+//
+//        scm {
+//            git "https://foo.com/some-group/${it}.git"
+//        }
+//
+//        steps {
+//            gradle('clean build')
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//// Factor out URL generation
+//Closure myGit(projectName) {
+//    return {
+//        git {
+//            remote {
+//                url "https://foo.com/some-group/${projectName}"
+//                credentials '30541887-abf1-4788-b071-d7c801f2beb5'
+//            }
+//        }
+//    }
+//}
+//
+//['some-repo', 'another-repo', 'yet-another-repo'].each {
+//    job("${it}-build-with-git-function") {
+//        wrappers {
+//            environmentVariables(
+//                    SOME_VARIABLE: 'someValue',
+//                    ANOTHER_VARIABLE: 'another-value'
+//            )
+//        }
+//
+//        scm myGit(it)
+//
+//        steps {
+//            gradle('clean build')
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//// Add stuff to a job
+//def myJob = job('my-job-integration')
+//SomeUtilities.integrationJob(myJob, '')
+//
+//
+//// Create a view
+//listView('integration-jobs') {
+//    description('All integration jobs')
+//
+//    jobs {
+//        names('my-job-integration', 'some-repo', 'test-job')
+//    }
+//}
